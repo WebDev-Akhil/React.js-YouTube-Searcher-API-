da357d53a564489d0c304ddaeae6b89409f1b8b8
Akhil Sarikonda <akhilascoder@gmail.com>
2019-07-23 20:19:26 +0530

    Added event to record input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,23 +8,24 @@
 
 // export default SearchBar;
 
-
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
-    return (<div className="ui segment">
-
-      <form className="ui form">
-        <div>
-          <label>Search Videos></label>
-          <input type="text" />
-
-        </div>
-      </form>
-
-
-    </div >);
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div>
+            <label>Search Videos></label>
+            <input type="text" onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
